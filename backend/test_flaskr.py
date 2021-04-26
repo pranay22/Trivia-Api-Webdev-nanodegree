d34dfca30478d1ba466353f5dc7038fee80d2a2b
Pranay Sarkar <pranaysarkar22@gmail.com>
2021-04-26 05:02:04 +0200

    tests fixed

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -16,6 +16,7 @@ class TriviaTestCase(unittest.TestCase):
         self.client = self.app.test_client
         self.database_name = "trivia_test"
         self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        #self.database_path = "postgresql:///trivia_test"
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -142,7 +143,7 @@ class TriviaTestCase(unittest.TestCase):
 
     # Error test: GET /categories/a/questions endpoint - invalid question in questions endpoint
     def test_failed_get_questions_per_category(self):
-        res = self.client().get('/categories/a/questions')
+        res = self.client().get('/categories/165/questions')
         self.assertEqual(res.status_code, 404)
         data = json.loads(res.data)
         self.assertEqual(data["success"], False)
