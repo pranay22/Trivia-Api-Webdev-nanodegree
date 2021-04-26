984232f5af8552f59f0215a12d537dd322416146
Pranay Sarkar <pranaysarkar22@gmail.com>
2021-04-26 04:20:00 +0200

    pep8 formatting more added

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -30,8 +30,10 @@ def create_app(test_config=None):
     '''
     @app.after_request
     def after_request(response):
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
-        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,PUT,POST,DELETE,OPTIONS')
         return response
 
     '''
